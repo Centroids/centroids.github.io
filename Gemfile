c67a471cb8df861379d926bbe7cb8da3aa557b68
centroid7@gmail.com <adelaro2@mail.sfsu.edu>
2024-01-05 14:25:36 -0800

    ruby tap music collage generator

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem 'httparty'
 gem 'pry-byebug'
 gem 'launchy'
 gem "dotenv", "~> 2.8"
+
--- test.rb
+++ test.rb
@@ -11,6 +11,51 @@ api_secret = ENV["LASTFM_SECRET"]
 
 lastfm = Lastfm.new(key, api_secret)
 
+# token = lastfm.auth.get_token
+# print "Hit return to open browser and authenticate..."
+# STDIN.gets 
+# system(%Q[open "http://www.last.fm/api/auth/?api_key=#{key}&token=#{token}"]) 
+# sleep(10)
+# lastfm.session = lastfm.auth.get_session(token: token)['key']
+# puts lastfm.session
+
+top_tracks = lastfm.user.get_top_tracks(user: 'Centroids')
+
+# # loop through tracks data for album art for each; .each_with_index for array indexation
+
+top_tracks.each_with_index do |track, idx|
+
+ begin
+    puts "Getting info for track #{idx + 1}"
+    info = lastfm.track.get_info(track: track["name"], artist: track['artist']['name'])
+    url = info["album"]["image"].last['content'] rescue 'about:blank'
+    track['url'] = url
+  rescue Lastfm::ApiError => e
+    puts "GetInfo returned exception: #{e.message}"
+  end
+end
+
+
+File.open("top_tracks.html", "w") do |f|
+ f.puts "<table>"
+ top_tracks.each do |track|
+   puts "Writing record for track: #{track}"
+   puts
+   f.puts "<tr>"
+   f.puts "<td>#{track['rank']}</td><td>#{track['name']}</td><td><img src='#{track['url']}'></td>"
+   f.puts "</tr>"
+ end
+ f.puts "</table>"
+end
+
+# https://lastfm.freetls.fastly.net/i/u/64s/8ee296bc1ea8dfcc004f9749da12b48b.jpg
+# https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png
+
+
+# lastfm.track.love(artist: 'Hujiko Pro', track: 'acid acid 7riddim')
+# lastfm.track.scrobble(artist: 'Hujiko Pro', track: 'acid acid 7riddim')
+# lastfm.track.update_now_playing(artist: 'Hujiko Pro', track: 'acid acid 7riddim')
+
 html_content = <<~HTML
   <html>
     <body>
@@ -30,49 +75,3 @@ File.write(temp_html_file, html_content)
 Launchy.open(temp_html_file)
 
 puts 'Waiting for user input. Close the browser window.'
-
-# token = lastfm.auth.get_token
-# print "Hit return to open browser and authenticate..."
-# STDIN.gets 
-# system(%Q[open "http://www.last.fm/api/auth/?api_key=#{key}&token=#{token}"]) 
-# sleep(10)
-# lastfm.session = lastfm.auth.get_session(token: token)['key']
-# puts lastfm.session
-
-top_tracks = lastfm.user.get_top_tracks(user: 'Centroids')
-
-# # loop through tracks data for album art for each; .each_with_index for array indexation
-
-#top_tracks.each_with_index do |track, idx|
-#
-# begin
-#    puts "Getting info for track #{idx + 1}"
-#    info = lastfm.track.get_info(track: track["name"], artist: track['artist']['name'])
-#    url = info["album"]["image"].last['content'] rescue 'about:blank'
-#    track['url'] = url
-#  rescue Lastfm::ApiError => e
-#    puts "GetInfo returned exception: #{e.message}"
-#  end
-#end
-
-
-#File.open("top_tracks.html", "w") do |f|
-#  f.puts "<table>"
-#  top_tracks.each do |track|
-#    puts "Writing record for track: #{track}"
-#    puts
-#    f.puts "<tr>"
-#    f.puts "<td>#{track['rank']}</td><td>#{track['name']}</td><td><img src='#{track['url']}'></td>"
-#    f.puts "</tr>"
-#  end
-#  f.puts "</table>"
-#end
-
-# https://lastfm.freetls.fastly.net/i/u/64s/8ee296bc1ea8dfcc004f9749da12b48b.jpg
-# https://lastfm.freetls.fastly.net/i/u/34s/2a96cbd8b46e442fc41c2b86b821562f.png
-
-
-# lastfm.track.love(artist: 'Hujiko Pro', track: 'acid acid 7riddim')
-# lastfm.track.scrobble(artist: 'Hujiko Pro', track: 'acid acid 7riddim')
-# lastfm.track.update_now_playing(artist: 'Hujiko Pro', track: 'acid acid 7riddim')
-
